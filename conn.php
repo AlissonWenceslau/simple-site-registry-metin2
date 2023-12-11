496650ce8c2daf294007ab01d6ff9d2fe8e6b83c
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-12-10 21:00:02 -0300

    modified conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
-    $ip_db = '192.168.1.150';
-    $user_db = 'User';
-    $password_db = '123';
+    $ip_db = 'IP DO SERVIDOR';
+    $user_db = 'USUÁRIO DO BANCO DE DADOS';
+    $password_db = 'SENHA DO BANCO DE DADOS';
     $database = 'account'; //Não alterar
 ?>
