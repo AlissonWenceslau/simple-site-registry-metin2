3cda810d33667f7b987fdda6f6cc57a51288ebc1
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2024-10-26 16:03:16 -0300

    Instruction conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
-    $ip_db = '127.0.0.1';
-    $user_db = 'root';
-    $password_db = '123';
+    $ip_db = 'IP DO SEU SERVIDOR';
+    $user_db = 'USUÁRIO DO BANCO DE DADOS';
+    $password_db = 'SENHA DO BANCO DE DADOS';
     $database = 'account'; //Não alterar
 ?>
